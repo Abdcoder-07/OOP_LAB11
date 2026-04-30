5057047dde6832b3f62504a4298c1a9d0f429ac5
Abdcoder-07 <k250868@gmail.com>
2026-04-30 23:14:00 +0500

    Add exerciase tasks

---
+++ exercises/task3.cpp
@@ -0,0 +1,39 @@
+// Store 5 student records in file
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Student {
+private:
+    int id;
+    string name;
+    float gpa;
+public:
+    void input() {
+        cout << "Enter ID: ";
+        cin >> id;
+        cin.ignore();
+        cout << "Enter Name: ";
+        getline(cin,name);
+        cout << "Enter GPA: ";
+        cin >> gpa;
+    }
+    void display() {
+        cout << "ID: " << id << endl;
+        cout << "Name: " << name << endl;
+        cout << "GPA: " << gpa << endl;
+    }
+    // Function to write object data to file
+    void writeToFile() {
+        ofstream file("task3.txt", ios::app);
+        file << id << " " << name << " " << gpa << endl;
+        file.close();
+    }
+};
+int main() {
+    Student s;
+    s.input();       // Take input
+    s.writeToFile(); // Save to file
+    cout << "Student record saved!" << endl;
+    return 0;
+}
